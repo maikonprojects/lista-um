abe12f8337dd80914ca5b06d4919ae52d2a82626
maikonprojects <162837965+maikonprojects@users.noreply.github.com>
2025-04-16 10:31:04 -0300

    Atualizando o 11

--- Exec11.java
+++ Exec11.java
@@ -9,9 +9,9 @@ public class Exec11 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        System.out.println("Digite a primeira frase: ");
+        System.out.println("Digite a primeira palavra: ");
         String frase1 = sc.nextLine();
-        System.out.println("Digite a primeira frase: ");
+        System.out.println("Digite a segunda palavra: ");
         String frase2 = sc.nextLine();
 
         List<String> listaFrase = new ArrayList<>();
@@ -23,9 +23,9 @@ public class Exec11 {
 
 
         if(frase1.compareTo(frase2) > 0){
-            System.out.println("A frase "+ frase1 +" é a maior das duas");
+            System.out.println("A palavra "+ frase1 +" é a maior das duas");
         }else{
-            System.out.println("A frase "+ frase2 +" é a maior das duas");
+            System.out.println("A palavra "+ frase2 +" é a maior das duas");
         }
     }
 }
